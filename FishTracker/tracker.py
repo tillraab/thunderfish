3ae86557681c847eba4190a30f4b1404611c5f08
Till Raab <till.raab@student.uni-tuebingen.de>
2016-06-30 11:27:28 +0200

    changes to analyse very long datasets. Further changes will apply soon.

--- FishTracker/tracker.py
+++ FishTracker/tracker.py
@@ -24,36 +24,50 @@ import time
 # function to load and combine the data...
 
 def pickle_save(fishes):
-
-    pickle.dump(fishes, open('%0.f.p' % (len(glob.glob("*.p"))+1), "wb"))
+    file = open('%0.f.p' % (len(glob.glob("*.p"))+1), "wb")
+    pickle.dump(fishes, file)
     # embed()
 
 def sort_fishes(all_fundamentals):
-    fishes = [[all_fundamentals[0][i]] for i in range(len(all_fundamentals[0]))]
-    last_fish_fundamentals = [fishes[i][-1] for i in range(len(fishes))]
+    fishes = [[ 0. ]]
+    last_fish_fundamentals = [ 0. ]
 
-    for list in range(1, len(all_fundamentals)):
-        for idx in range(len(all_fundamentals[list])):
-            diff = abs(np.asarray(last_fish_fundamentals) - all_fundamentals[list][idx])
-            if diff[np.argsort(diff)[0]] < 1 and diff[np.argsort(diff)[0]] > -1:
-                fishes[np.argsort(diff)[0]].append(all_fundamentals[list][idx])
-                last_fish_fundamentals[np.argsort(diff)[0]] = all_fundamentals[list][idx]
-            else:
-                fishes.append([np.nan for i in range(list)])
-                fishes[-1].append(all_fundamentals[list][idx])
-                last_fish_fundamentals.append(all_fundamentals[list][idx])
+    # loop throught lists of fundamentals detected in sequenced PSDs.
+    for list in range(len(all_fundamentals)):
+        # if this list is empty add np.nan to every deteted fish list.
+        if len(all_fundamentals[list]) == 0:
+                for fish in range(len(fishes)):
+                    fishes[fish].append(np.nan)
+        # when list is not empty...
+        else:
+            # ...loop trought every fundamental for each list
+            for idx in range(len(all_fundamentals[list])):
+                # calculate the difference to all last detected fish fundamentals
+                diff = abs(np.asarray(last_fish_fundamentals) - all_fundamentals[list][idx])
+                # find the fish where the frequency fits bests (th = 1Hz)
+                if diff[np.argsort(diff)[0]] < 1 and diff[np.argsort(diff)[0]] > -1:
+                    # add the frequency to the fish array and update the last_fish_fundamentals list
+                    fishes[np.argsort(diff)[0]].append(all_fundamentals[list][idx])
+                    last_fish_fundamentals[np.argsort(diff)[0]] = all_fundamentals[list][idx]
+                # if its a new fish create a list of nans with the frequency in the end (list has same length as other
+                # lists.) and add frequency to last_fish_fundamentals
+                else:
+                    fishes.append([np.nan for i in range(list)])
+                    fishes[-1].append(all_fundamentals[list][idx])
+                    last_fish_fundamentals.append(all_fundamentals[list][idx])
 
         # wirte an np.nan for every fish that is not detected in this window!
         for fish in range(len(fishes)):
             if len(fishes[fish]) != list +1:
                 fishes[fish].append(np.nan)
-        # need method to assing fish with same frequency as already known to a new fish when it has been gone for to long
 
     # reshape everything to arrays
     for fish in range(len(fishes)):
         fishes[fish] = np.asarray(fishes[fish])
-    return fishes
+    # remove first fish because it has beeen used for the first comparison !
+    fishes.pop(0)
 
+    return fishes
 
 def main(audio_file):
     cfg = ct.get_config_dict()
@@ -70,11 +84,9 @@ def main(audio_file):
     # fig, ax = plt.subplots()
     idx = 0
     all_fundamentals = []
-
+    t0 = time.time()
     while idx < int((len(data)-8*samplrate) / samplrate):
-        if idx > 1200:
-            quit() # quit at 20 minutes !!!
-        t0 = time.time()
+
         tmp_data = data[idx*samplrate : (idx+8)*samplrate]
         psd_data = ps.multi_resolution_psd(tmp_data, samplrate)
         fishlist = hg.harmonic_groups(psd_data[1], psd_data[0], cfg)[0]
@@ -85,23 +97,21 @@ def main(audio_file):
             # ax.plot(np.ones(len(fundamentals)) * idx, fundamentals, 'k.')
             # plt.draw()
             # plt.pause(0.001)
+        else:
+            all_fundamentals.append(np.array([]))
+
         # print idx
-        if idx % 60 < 0.1 and not idx == 0.0:
+        if idx % 1800 < 0.1 and not idx == 0.0:
+            print('%.1f sec; Processing 30 min took %.2f sec.' % (idx, time.time() - t0))
+            t0 = time.time()
+            # print idx
             fishes = sort_fishes(all_fundamentals)
             # pickle save function
             pickle_save(fishes)
-
             all_fundamentals = []
-            # embed()
-            # quit()
- # fishes[0][~np.isnan(fishes[0])]    gets the elements that are not nan !!!
-        idx += 0.1
 
-        print idx, "%.2f sec" % (time.time() - t0)
+        idx += 0.1
     fishes = sort_fishes(all_fundamentals)
-    # plt.show()
-    # embed()
-    # quit()
 
 if __name__ == '__main__':
     print('TRACK ALL THE FISHES')
